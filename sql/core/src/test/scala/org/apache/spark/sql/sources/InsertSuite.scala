54fab6889b6ae2ac456e58b42a7e1508a639fa45
ksbeyer <168490557+ksbeyer@users.noreply.github.com>
2025-07-25 14:03:28 -0700

    [SPARK-52772] Fix handling of empty metadata in removeRedundantAlias and trimNonTopLevelAliases. Fix applyColumnMetadata to properly flow table column metadata and block source metadata. Add tests in InsertSuite for cases when source metadata leaked before these changes.

--- sql/core/src/test/scala/org/apache/spark/sql/sources/InsertSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/sources/InsertSuite.scala
@@ -1974,7 +1974,6 @@ class InsertSuite extends DataSourceTest with SharedSparkSession {
   test("SPARK-40001 JSON DEFAULT columns = JSON_GENERATOR_WRITE_NULL_IF_WITH_DEFAULT_VALUE off") {
     // Check that the JSON_GENERATOR_WRITE_NULL_IF_WITH_DEFAULT_VALUE config overrides the
     // JSON_GENERATOR_IGNORE_NULL_FIELDS config.
-    // JSON feature created with https://issues.apache.org/jira/browse/SPARK-40001
     withSQLConf(SQLConf.JSON_GENERATOR_WRITE_NULL_IF_WITH_DEFAULT_VALUE.key -> "true",
       SQLConf.JSON_GENERATOR_IGNORE_NULL_FIELDS.key -> "true") {
       withTable("t") {
